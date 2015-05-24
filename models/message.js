ddf427af5db7502a73e1bd4fb3eb365f68b7a5bb
KevinBacas <bacas.kevin@hotmail.fr>
2015-05-24 23:32:07 +0200

    Added QQ method to Message model

--- models/message.js
+++ models/message.js
@@ -1,13 +1,25 @@
 module.exports = (function(){
   'use strict';
 
+  // Requirements
   var mongoose = require('mongoose');
   var config = require('../config');
   var Schema = mongoose.Schema;
 
-  var Message = mongoose.model('Message', new Schema({
-    content: String
-  }));
+  // Setting up the Schema
+  var MessageSchema = new Schema({
+    content: String,
+    date: { type: Date, default: Date.now }
+  });
 
+  // Adding methods
+  MessageSchema.methods.QQ = function(){
+    return this.content || 'QQ';
+  };
+
+  // Creating model
+  var Message = mongoose.model('Message', MessageSchema);
+
+  // Returning the final object
   return Message;
 })();
