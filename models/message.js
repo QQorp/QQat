a163f254fa81d927a700f8b12a64038ea77793c9
KevinBacas <bacas.kevin@hotmail.fr>
2015-05-26 00:02:26 +0200

    Models: Message: Added a relation to channel

--- models/message.js
+++ models/message.js
@@ -6,17 +6,23 @@ module.exports = (function(){
   var config = require('../config');
   var Schema = mongoose.Schema;
   var User = require('./user');
+  var Channel = require('./channel');
 
   // Setting up the Schema
   var MessageSchema = new Schema({
     content: String,
     sender: { type: Schema.Types.ObjectId, ref: 'User', default: null },
+    channel: { type: Schema.Types.ObjectId, ref: 'Channel', default: null },
     date: { type: Date, default: Date.now }
   });
 
   // Adding methods
   MessageSchema.methods.getSenderName = function(){
     return this.sender ? this.sender.username : null;
+  };
+
+  MessageSchema.methods.getChannelName = function(){
+    return this.channel ? this.channel.label : null;
   };
 
   // Creating model
