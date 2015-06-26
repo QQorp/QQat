a68ac0dfd17d1ad90057914809f4eab44f3cbc0a
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-26 20:41:17 +0200

    Channel: Added GetChannel function and related tests

--- models/channel.go
+++ models/channel.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/nu7hatch/gouuid"
 )
 
@@ -38,4 +39,26 @@ func CreateChannel(channelName string) (*Channel, error) {
 	}
 
 	return nil, fmt.Errorf("Channel's name cannot be empty")
+}
+
+// GetChannel : Returns a channel if exists
+func GetChannel(channelUID string) (*Channel, error) {
+	if channelUID != "" {
+		c := RedisPool.Get()
+		defer c.Close()
+
+		foundNumber, err := redis.Int(c.Do("SISMEMBER", "Channels", "Channel:"+channelUID))
+		if err == nil && foundNumber == 1 {
+			channelName, err := redis.String(c.Do("HGET", "Channel:"+channelUID, "Name"))
+			if err == nil {
+				channel := &Channel{
+					ChannelUID:  channelUID,
+					ChannelName: channelName,
+				}
+				return channel, nil
+			}
+		}
+		return nil, err
+	}
+	return nil, fmt.Errorf("ChannelUID not provided")
 }
--- tests/channel_test.go
+++ tests/channel_test.go
@@ -20,32 +20,52 @@ func init() {
 func TestChannel(t *testing.T) {
 	Convey("Testing channel model", t, func() {
 		Reset(ResetRedis)
-		Convey("Creating a channel with blank name, should return an error", func() {
-			channel, err := CreateChannel("")
+		Convey("Testing CreateChannel", func() {
+			Convey("Creating a channel with blank name, should return an error", func() {
+				channel, err := CreateChannel("")
 
-			So(channel, ShouldBeNil)
-			So(err, ShouldNotBeNil)
+				So(channel, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+			})
+			Convey("Creating a channel with a name, should return a Channel", func() {
+				channelName := "QQ"
+				channel, err := CreateChannel(channelName)
+
+				So(channel, ShouldNotBeNil)
+				So(channel.ChannelName, ShouldEqual, channelName)
+				So(err, ShouldBeNil)
+			})
+			Convey("Creating two channels with the same name, should return different channels", func() {
+				channelName := "QQ"
+				channel1, err1 := CreateChannel(channelName)
+				channel2, err2 := CreateChannel(channelName)
+
+				So(channel1, ShouldNotBeNil)
+				So(channel2, ShouldNotBeNil)
+				So(channel1.ChannelName, ShouldEqual, channelName)
+				So(channel1.ChannelName, ShouldEqual, channel2.ChannelName)
+				So(channel1.ChannelUID, ShouldNotEqual, channel2.ChannelUID)
+				So(err1, ShouldBeNil)
+				So(err2, ShouldBeNil)
+			})
 		})
-		Convey("Creating a channel with a name, should return a Channel", func() {
-			channelName := "QQ"
-			channel, err := CreateChannel(channelName)
+		Convey("Testing GetChannel", func() {
+			Convey("Getting channel with an empty string, should return error", func() {
+				channel, err := GetChannel("")
 
-			So(channel, ShouldNotBeNil)
-			So(channel.ChannelName, ShouldEqual, channelName)
-			So(err, ShouldBeNil)
-		})
-		Convey("Creating two channels with the same name, should return different channels", func() {
-			channelName := "QQ"
-			channel1, err1 := CreateChannel(channelName)
-			channel2, err2 := CreateChannel(channelName)
+				So(channel, ShouldBeNil)
+				So(err, ShouldNotBeNil)
+			})
+			Convey("Creating a channel and getting it, should return the channel", func() {
+				channelName := "lel"
+				createdChannel, _ := CreateChannel(channelName)
+				channel, err := GetChannel(createdChannel.ChannelUID)
 
-			So(channel1, ShouldNotBeNil)
-			So(channel2, ShouldNotBeNil)
-			So(channel1.ChannelName, ShouldEqual, channelName)
-			So(channel1.ChannelName, ShouldEqual, channel2.ChannelName)
-			So(channel1.ChannelUID, ShouldNotEqual, channel2.ChannelUID)
-			So(err1, ShouldBeNil)
-			So(err2, ShouldBeNil)
+				So(channel, ShouldNotBeNil)
+				So(err, ShouldBeNil)
+				So(channel.ChannelName, ShouldEqual, channelName)
+				So(channel.ChannelUID, ShouldEqual, createdChannel.ChannelUID)
+			})
 		})
 	})
 }
