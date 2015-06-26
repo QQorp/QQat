15a942ca54b232e2b69ab2e53bb175a31657292e
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-26 12:03:46 +0200

    Models: Channel: Added Redis calls and tests still pass :heart:

--- models/channel.go
+++ models/channel.go
@@ -3,8 +3,25 @@ package models
 import (
 	"fmt"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/nu7hatch/gouuid"
 )
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 12000,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
+}
+
+var pool = newPool()
 
 // Channel : Correponds to a Channel
 type Channel struct {
@@ -13,18 +30,29 @@ type Channel struct {
 }
 
 // CreateChannel : Creates a channel and add it to redis
-func CreateChannel(channelName string) (channel *Channel, err error) {
-	channel = nil
-	err = fmt.Errorf("Channel name cannot be empty")
+func CreateChannel(channelName string) (*Channel, error) {
 	if channelName != "" {
-		u, _ := uuid.NewV4()
-		channel = &Channel{
-			ChannelName: channelName,
-			ChannelUID:  u.String(),
-		}
+		u, err := uuid.NewV4()
+		if err == nil {
+			channel := &Channel{
+				ChannelName: channelName,
+				ChannelUID:  u.String(),
+			}
 
-		// No error until the end, setting err to nil for return
-		err = nil
+			// Adding channel to Redis
+			c := pool.Get()
+			defer c.Close()
+
+			_, err := c.Do("SADD", "Channels", "Channel:"+channel.ChannelUID)
+			if err == nil {
+				_, err := c.Do("HMSET", "Channel:"+channel.ChannelUID, "Name", channel.ChannelName)
+				if err == nil {
+					return channel, nil
+				}
+			}
+		}
+		return nil, err
 	}
-	return channel, err
+
+	return nil, fmt.Errorf("Channel's name cannot be empty")
 }
