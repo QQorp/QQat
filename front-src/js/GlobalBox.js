5838bd40a1fbdcc5ca52961cddd66fa5b22a7aab
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-28 12:59:47 +0200

    front-src: Fixed bootstrap page width

--- front-src/js/GlobalBox.js
+++ front-src/js/GlobalBox.js
@@ -5,17 +5,18 @@ import MessageBox from './MessageBox';
 var GlobalBox = React.createClass({
     render: function() {
         return (
+          <div className="container-fluid">
             <div className="row">
-                <div id="panel" className="col-md-3 no-float">
-                    <h3><i className="fa fa-bars"></i> Channels :</h3>
-                    <Channel url="/channels/" />
-                    <h3><i className="fa fa-user"></i> Users :</h3>
-                    <Channel url="/users" />
-                </div>
+              <div id="panel" className="col-md-3 no-float">
+                <h3><i className="fa fa-bars"></i> Channels :</h3>
+                <Channel url="/channels/" />
+                <h3><i className="fa fa-user"></i> Users :</h3>
+                <Channel url="/users" />
+              </div>
 
-                <MessageBox />
+              <MessageBox />
             </div>
-
+          </div>
         );
     }
 });
