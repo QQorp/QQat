89e9e9afe27673611dbab25bb75e0e1b52c2a666
kevinbacas <bacas.kevin@hotmail.fr>
2015-06-30 12:06:01 +0200

    Routers: Binded mock to actual api

--- routers/commentsRouter_github_com_QQorp_QQat_controllers.go
+++ routers/commentsRouter_github_com_QQorp_QQat_controllers.go
@@ -6,6 +6,34 @@ import (
 
 func init() {
 	
+	beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"] = append(beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"],
+		beego.ControllerComments{
+			"Post",
+			`/`,
+			[]string{"post"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"] = append(beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"],
+		beego.ControllerComments{
+			"Get",
+			`/:uid`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"] = append(beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"],
+		beego.ControllerComments{
+			"GetAll",
+			`/`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"] = append(beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:FakeChannelController"],
+		beego.ControllerComments{
+			"GetAllMessages",
+			`/:uid/messages`,
+			[]string{"get"},
+			nil})
+
 	beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:ChannelController"] = append(beego.GlobalControllerRouter["github.com/QQorp/QQat/controllers:ChannelController"],
 		beego.ControllerComments{
 			"Post",
