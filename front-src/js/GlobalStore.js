ac13445edffeae3792f1e2f1eade57817f997b96
kevinbacas <bacas.kevin@hotmail.fr>
2015-07-02 13:06:29 +0200

    Front-end: Added mock for localStorage saving

--- front-src/js/GlobalStore.js
+++ front-src/js/GlobalStore.js
@@ -15,6 +15,7 @@ var GlobalStore = {
       success: function(data) {
         GlobalStore.state.channels = data;
         GlobalStore.onChannelsLoaded(data);
+        GlobalStore.save();
         if(GlobalStore.state.currentChannel === '' && data.length > 0) {
           GlobalStore.setCurrentChannel(data[0].ChannelUID);
           GlobalStore.loadMessages(GlobalStore.getCurrentChannel());
@@ -30,6 +31,7 @@ var GlobalStore = {
     if(new_channel_name !== this.state.currentChannel) {
       this.state.currentChannel = new_channel_name;
       this.onNewCurrentChannel(new_channel_name);
+      this.save();
     }
   },
 
@@ -45,13 +47,26 @@ var GlobalStore = {
       success: function(data) {
         GlobalStore.state.messages[channel_uid] = data;
         GlobalStore.onMessagesLoaded(channel_uid);
+        GlobalStore.save();
       }.bind(this),
       error: function(xhr, status, err) {
         console.log(GlobalStore.state);
       }.bind(this)
     });
+  },
+
+  save: function() {
+    console.log("Saving data...");
+    console.log("Saving data... DONE!");
+  },
+
+  load: function() {
+    console.log("Loading data...");
+    console.log("Loading data... DONE!");
   }
 
 };
+
+GlobalStore.load();
 
 module.exports = GlobalStore;
